4e98400cbe120b40ba9e3d17b52982d3abb6aac5
Sharanya-dev-cmd <sharanya.sinha2012@gmail.com>
2026-06-02 00:09:26 -0700

    Add suspicious_link_detector_v1

---
+++ suspicious_link_detector_v1.py
@@ -0,0 +1,39 @@
+# suspicious link detector v1
+
+# ask user for link
+link = input("enter a website link : ")
+
+# predetermined score
+score = 0
+
+# give intel to train the detector
+has_tinyurl = "tinyurl" in link
+has_bitly = "bit.ly" in link
+free = "free" in link
+money = "money" in link
+
+# checking for scammy lines
+if has_tinyurl == True:
+    score = score + 1
+    print("'tinyurl' detected")
+
+if has_bitly == True:
+    score = score + 1
+    print("'bit.ly' detected")
+
+if free == True:
+    score = score + 1
+    print("'free' detected")
+
+if money == True:
+    score = score + 1
+    print("'money' detected")
+
+
+# counting final score to check possibilities of suspicious links
+if score <= 1 :
+    print("scam possibility : LOW")
+elif score == 2 or score == 3 :
+    print("scam possibility : MEDIUM")
+else :
+    print("scam possibility : HIGH")
